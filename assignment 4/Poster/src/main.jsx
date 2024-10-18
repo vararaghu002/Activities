101bf0fdd21abee56f3b6f5fc7b448029fd4d431
Raghu Ram Vara <vararaghuram002@gmail.com>
2024-10-18 17:02:19 +0530

    updated

---
+++ assignment 4/Poster/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {RouterProvider,createBrowserRouter} from 'react-router-dom';
+
+import Posts, {loader as postsLoader} from './routes/Posts.jsx'
+import './index.css'
+import NewPost, {action as newPostAction} from './routes/NewPost.jsx';
+import RootLayout from './routes/RootLayout.jsx'
+import PostDetails,{loader as PostDetailsLoader} from './routes/PostDetails.jsx';
+const router = createBrowserRouter([
+  
+
+  {path:'/',
+    element:<RootLayout />,
+    children:[
+    { path:'/',element:<Posts />,
+      loader:postsLoader,
+      children:[{path:'/create-post',element:<NewPost />,action:newPostAction },
+        {path:'/:id', element:<PostDetails />,loader:PostDetailsLoader}
+    ],},
+    ],
+},
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
+
